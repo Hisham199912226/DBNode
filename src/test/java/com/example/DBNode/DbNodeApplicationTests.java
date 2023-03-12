67a17c7f01cb4f5ea82ac4657ad20f2ae37c2b98
hisham99 <hisham.aleesa@gmail.com>
2023-03-12 19:55:50 +0300

    initial commit

---
+++ src/test/java/com/example/DBNode/DbNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DBNode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
